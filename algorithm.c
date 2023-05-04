774eaac4ddbc574cee524c0b2b12bd5154816185
Andrés Abundis <andreabundis@gmail.com>
2023-05-03 20:50:27 -0500

    Open files to load matrix.

--- algorithm.c
+++ algorithm.c
@@ -6,6 +6,45 @@
 #include <emmintrin.h>
 #include <immintrin.h>
 
+// CONSTANTS
+#define MAX_LINE_LENGTH 1000
+
+// FUNCTIONS
+int OpenFile(char id_matrix) {
+    printf("Opening file: ");
+    FILE    *textfile;
+    char    line[MAX_LINE_LENGTH];
+    
+    if (id_matrix == 'A'){
+        printf("A\n");
+        textfile = fopen("matrix/matrixA2500.txt", "r");
+    } else if (id_matrix == 'B') {
+        printf("B\n");
+        textfile = fopen("matrix/matrixB2500.txt", "r");
+    }
+    
+    if(textfile == NULL) {
+        printf("Null file for matrix %c, try again!\n", id_matrix);
+        return 0;
+    }
+    
+    // while(fgets(line, MAX_LINE_LENGTH, textfile)){
+    //     printf(line);
+    // }
+    
+    fclose(textfile);
+    printf("Successful load of matrix %c\n", id_matrix);
+    return 1;
+}
+
+int LoadMatrixes() {
+    if(!OpenFile('A') || !OpenFile('B')) {
+        printf("Error while loading matrixes.");
+        return 0;
+    }
+    return 1;
+}
+
 void CreateTable(){
     printf ("| Run  || Serial | Parallel 1 | Parallel 2 | \n");  
     for (int i = 0; i < 5; i++)  {  
@@ -15,9 +54,7 @@ void CreateTable(){
     printf ("| % vs Serial  || -- | %d | %d | \n"); //Cambiar a datos guardados
 }
 
-
 int main(){
-
-
+    int res = LoadMatrixes();
     return 0;
 }
