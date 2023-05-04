ee3613e194d4bcd70c9ae2f8d125cd566138c206
PauMarquez99 <pauilimq99@gmail.com>
2023-05-03 20:18:06 -0500

    tableTemplate

--- algorithm.c
+++ algorithm.c
@@ -1,8 +1,19 @@
+//Functions with camel, variables with underscore
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <emmintrin.h>
 #include <immintrin.h>
+
+void CreateTable(){
+    printf ("| Run  || Serial | Parallel 1 | Parallel 2 | \n");  
+    for (int i = 0; i < 5; i++)  {  
+        printf ("| %d || %d | %d | %d | \n ", i, i, i, i); //Cambiar a datos guardados   
+    }  
+    printf ("| Average  || %d | %d | %d | \n"); //Cambiar a datos guardados
+    printf ("| % vs Serial  || -- | %d | %d | \n"); //Cambiar a datos guardados
+}
 
 
 int main(){
